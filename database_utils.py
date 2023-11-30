d53223b3238b67476b2bf94f002dae75cb23b5d9
Ava Logan <ava2logan@gmail.com>
2023-11-30 21:55:57 +0000

    Correctly import yaml file

--- database_utils.py
+++ database_utils.py
@@ -1,21 +1,30 @@
 import yaml 
+with open('db_creds.yaml', 'r') as file:
+    data = yaml.safe_load(file)
+
+import sqlalchemy as sa
+import pandas as pd
+from sa import create_engine
 
 class DatabaseConnector:
     #used to connect and upload data to database
 
-    def read_db_creds():
+    def read_db_creds(data):
     #read creds in yaml and return dictionary 
-    creds_dict = {}
-    for key, value in yaml.load(open('db_creds.yaml')).iteritems():
-        creds_dict[key]=value
-    return creds_dict    
+        creds_dict = {}
+        for key, value in data:
+            creds_dict[key]=value
+        return creds_dict    
 
-    def init_db_engine():
-        import sqlalchemy as sa 
-        sa.engine.url.URL = sa.engine.URL.create(creds_dict)
-    return sa.engine.url.URL
+    #def init_db_engine(creds_dict):
+        #engine = create_engine(f"{RDS_DATABASE}+{DBAPI}://{username}:{password}@{host}:{port}/{database_name}")
+        #reads read_db_cred and initialise and returns an sqalchemy database engine 
 
-    #reads read_db_cred and initialise and returns an sqalchemy database engine 
+    #def list_db_tables():
+        #from sqlalchemy import inspect 
+        #inspector = inspect(engine)
+        #table_names = inspector.get_table_names()
+        #return table_names
 
-    def list_db_tables():
-    #useds engine from init_db_engine to list all tables 
+    #useds engine from init_db_engine to list all tables
+
