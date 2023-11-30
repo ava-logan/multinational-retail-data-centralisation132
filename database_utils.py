bdc2b115136b262aa61cf1ada9c450c8c35d7dca
Ava Logan <ava2logan@gmail.com>
2023-11-30 22:14:04 +0000

    list_db_tables method updated

--- database_utils.py
+++ database_utils.py
@@ -9,22 +9,22 @@ from sqlalchemy import create_engine
 class DatabaseConnector:
     #used to connect and upload data to database
 
-    def read_db_creds(data):
+    def read_db_creds(self, data):
     #read creds in yaml and return dictionary 
         creds_dict = {}
         for key, value in data:
             creds_dict[key]=value
         return creds_dict    
 
-    def init_db_engine(data):
+    def init_db_engine(self, data):
         engine = create_engine(data)
         #reads read_db_cred and initialise and returns an sqalchemy database engine 
 
-    #def list_db_tables():
-        #from sqlalchemy import inspect 
-        #inspector = inspect(engine)
-        #table_names = inspector.get_table_names()
-        #return table_names
+    def list_db_tables(self, init_db_engine):
+        from sqlalchemy import inspect 
+        inspector = inspect(init_db_engine.engine)
+        table_names = inspector.get_table_names()
+        return table_names
 
     #useds engine from init_db_engine to list all tables
 
