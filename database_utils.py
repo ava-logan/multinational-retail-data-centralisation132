f916e77b2b8992066a561e30144bfdf8c1aa963e
Ava Logan <ava2logan@gmail.com>
2023-11-30 23:38:55 +0000

    Creating an engine

--- database_utils.py
+++ database_utils.py
@@ -11,26 +11,54 @@ class DatabaseConnector:
         self.data = data
     #used to connect and upload data to database
 
-    def read_db_creds(self, data):
-    #read creds in yaml and return dictionary 
+    def read_db_creds(self):
+    #read creds in yaml and return dictionary
+    #print(type(db_creds)) returns dict type so i dont know why i need this  
         creds_dict = {}
-        for key, value in self.data:
-            creds_dict[key]=value
+        for key, value in self.data.items():
+            creds_dict[key] = value
         return creds_dict    
 
-    def init_db_engine(self, data, read_db_creds):
-        engine_creds_dict = read_db_creds(self.data)
-        engine = create_engine(engine_creds_dict)
-        print(engine)
+    def init_db_engine(self):
+        #required inputs to create an engine 
+        #password included here - gitignore?
+        #wouldnt work if the keys were not as expected 
+        #seems silly to create a method but would be useful for automation
+        database_type = 0
+        dbapi = 0
+        host = 0 
+        user = 0
+        password = 0 
+        database = 0
+        port = 5432
+        for key,value in self.data.items():
+            if key == "DATABASE_TYPE":
+                database_type = value
+            if key == "DBAPI":
+                dbapi = value 
+            if key == "HOST":
+                host = value 
+            if key == "USER":
+                user = value
+            if key == "PASSWORD":
+                password = value  
+            if key == "DATABASE":
+                database = value   
+            if key == "PORT":
+                port = value    
+
+        engine = create_engine(f"{database_type}+{dbapi}://{user}:{password}@{host}:{port}/{database}")
+        #engine.connect()
+        #print(engine)
         #reads read_db_cred and initialise and returns an sqalchemy database engine 
 
-    def list_db_tables(self, data, init_db_engine):
+    def list_db_tables(self):
         from sqlalchemy import inspect 
         inspector = inspect(init_db_engine.engine)
         table_names = inspector.get_table_names()
         return table_names
             #useds engine from init_db_engine to list all tables
 
-print(db_creds)
-#yaml_engine = DatabaseConnector(creds_data)
-#yaml_engine.read_db_creds()
+
+yaml_engine = DatabaseConnector(db_creds)
+yaml_engine.init_db_engine()
