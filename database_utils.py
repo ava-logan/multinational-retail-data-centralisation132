4f067561f87dbb74fb089347af9e4557b8dc8fcb
Ava Logan <ava2logan@gmail.com>
2023-11-30 22:57:10 +0000

    Error fix - yaml file (not correctly saved using vscode)

--- database_utils.py
+++ database_utils.py
@@ -1,30 +1,36 @@
 import yaml 
-with open('db_creds.yaml', 'r') as file:
-    data = yaml.safe_load(file)
-
 import sqlalchemy as sa
 import pandas as pd
 from sqlalchemy import create_engine
 
+with open('db_creds.yaml', 'r') as file:
+    db_creds = yaml.safe_load(file)
+
 class DatabaseConnector:
+    def __init__(self, data):
+        self.data = data
     #used to connect and upload data to database
 
     def read_db_creds(self, data):
     #read creds in yaml and return dictionary 
         creds_dict = {}
-        for key, value in data:
+        for key, value in self.data:
             creds_dict[key]=value
         return creds_dict    
 
-    def init_db_engine(self, data):
-        engine = create_engine(data)
+    def init_db_engine(self, data, read_db_creds):
+        engine_creds_dict = read_db_creds(self.data)
+        engine = create_engine(engine_creds_dict)
+        print(engine)
         #reads read_db_cred and initialise and returns an sqalchemy database engine 
 
-    def list_db_tables(self, init_db_engine):
+    def list_db_tables(self, data, init_db_engine):
         from sqlalchemy import inspect 
         inspector = inspect(init_db_engine.engine)
         table_names = inspector.get_table_names()
         return table_names
+            #useds engine from init_db_engine to list all tables
 
-    #useds engine from init_db_engine to list all tables
-
+print(db_creds)
+#yaml_engine = DatabaseConnector(creds_data)
+#yaml_engine.read_db_creds()
