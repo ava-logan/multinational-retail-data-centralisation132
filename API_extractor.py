c166a0759b465a3faf36f4eec4609c56741ff582
Ava Logan <ava2logan@gmail.com>
2023-12-13 23:58:42 +0000

    Added column names

--- API_extractor.py
+++ API_extractor.py
@@ -9,21 +9,31 @@ def list_number_of_stores(url, header):
     print(data)
 
 #list_number_of_stores(store_numbers, api_key)  #response 451
+class CreateDataframe:
+    def retrieve_stores_data(header):
+        import pandas as pd
+        column_names = CreateDataframe.get_column_names(header)
+        my_dataframe = pd.DataFrame()
+        for x in range(2):
+            store_finder = f'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{x}'
+            response = requests.get(store_finder, headers=header)
+            data = response.json()
+            new_data = []
+            for key, value in data.items():
+                new_data.append(value)
+            ready_to_add = pd.DataFrame([new_data], columns=column_names)
+            my_dataframe = pd.concat([my_dataframe, ready_to_add], ignore_index=True)
+        print(my_dataframe)
 
-def retrieve_stores_data(header):
-    import pandas as pd
-    my_dataframe = pd.DataFrame()
-    for x in range(2):
-        store_finder = f'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{x}'
-        response = requests.get(store_finder, headers=header)
+    def get_column_names(header):
+        source = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/1'
+        response = requests.get(source, headers=header)
         data = response.json()
-        new_data = []
+        column_names = []
         for key, value in data.items():
-            new_data.append(value)
-        ready_to_add = pd.DataFrame([new_data], columns=None)
-        my_dataframe = pd.concat([my_dataframe, ready_to_add], ignore_index=True)
-    print(type(my_dataframe))
+                column_names.append(key)
+        return column_names
 
 
-retrieve_stores_data(api_key)
+CreateDataframe.retrieve_stores_data(api_key)
 
