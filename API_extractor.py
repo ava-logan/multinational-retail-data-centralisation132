0947ea3a6208bdd95777bc64f175d786ff717510
Ava Logan <ava2logan@gmail.com>
2023-12-13 09:25:58 +0000

    api token success

--- API_extractor.py
+++ API_extractor.py
@@ -1,18 +1,19 @@
 import requests
-api_key = {'api_key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
-store_finder = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}'
+api_key = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+store_finder = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/5'
 store_numbers = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
 
 def list_number_of_stores(url, header):
-    response = requests.get(url, header)
-    print(response.status_code)
+    response = requests.get(url, headers=header)
+    data = response.json()
+    print(data)
 
-#list_number_of_stores(store_numbers, api_key)  #response 403
+#list_number_of_stores(store_numbers, api_key)  #response 451
 
 def retrieve_stores_data(url, header):
-    response = requests.get(url, header)
-    print(response.status_code)
+    response = requests.get(url, headers=header)
+    data = response.json()
+    print(data)
 
-
-response = requests.get('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores?api_key=yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX')
-print(response.status_code)
+retrieve_stores_data(store_finder, api_key)
+    
